c75bbefede8fa43f0a18d96799a6f2fc7ab48bc7
Saastha Vasan <saasthavasan@gmail.com>
2019-03-26 20:54:36 +0530

    Modified decryption.py

--- decryption.py
+++ decryption.py
@@ -1,10 +1,10 @@
 from Crypto.Cipher import AES
 
 
-def Decrypt(encryptedData):
+def decrypt(encryptedData):
 
 # Extracted key from Jigsaw Ransomware
-    key = ':\x82,\x03\x0c\x05\xdbw\x08\t\n\x0b\x0c\r\x0e\r'
+    key = ':\x82,\x03\x0c\x05\xdb\x08\t\n\x0b\x0c\r\x0e\r'
 # Extracted Initialization Vector(iv) from the Jigsaw
     iv = '\x00\x01\x00\x03\x05\x03\x00\x01\x00\x00\x02\x00\x06\x07\x06\x00'
 
@@ -13,3 +13,18 @@ def Decrypt(encryptedData):
     decryptedData = obj.decrypt(encryptedData)
 
     return decryptedData
+
+
+def decryptFile(path)
+	try:
+		inputFile = path
+		encryptedData = readFile(inputFile)
+		decryptedData = decrypt(encryptedData)
+		writeFile(inputFile, decryptedData)
+		return 1
+	except:
+		print('jigsawRansomware.py -<option>')
+		print('For help command is:')
+		print('jigsawRansomware.py -h')
+                return 0
+
