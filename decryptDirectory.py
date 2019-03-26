bf007060846412d32914e2113d90cfb119695fbf
Saastha Vasan <saasthavasan@gmail.com>
2019-03-26 20:47:35 +0530

    Added decryptDirectory.py

---
+++ decryptDirectory.py
@@ -0,0 +1,29 @@
+import Decrypt.py
+import os
+
+
+
+
+
+def directoryDecrypt(path):
+	if os.version == 'nt':
+		dirListing = os.listdir(path)
+		fileList = []
+		counter = 0
+		for item in dirListing:
+			if ".fun" in item:
+			fileList.append(path+'\\'+item)
+		for item in fileList:
+			ret = decryptFile(item)
+			if ret == 1:
+				print("Decrypted " + item)
+				counter = counter + 1
+			else:
+				print("Unable to decrypt" + item)
+		if counter == len(fileList):
+			return 1
+		else:
+			return 0
+	else:
+		print("Unsupported OS Version")
+		return 0
