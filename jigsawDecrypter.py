95c7fc81e9872ff0993f323683279064c869eb77
Saastha Vasan <saasthavasan@gmail.com>
2019-03-26 18:49:49 +0530

    Decryption File Modified

--- jigsawDecrypter.py
+++ jigsawDecrypter.py
@@ -1,45 +1,57 @@
 import sys, getopt
 import decryption
+import registryRemover
+
 
 def readFile(inputFile):
 
-    inFile = open(inputFile, 'r')
-    encryptedData = inFile.read()
-    inFile.close()
+	inFile = open(inputFile, 'r')
+	encryptedData = inFile.read()
+	inFile.close()
 
-    return encryptedData
+	return encryptedData
 
 
 def writeFile(inputFile, decryptedData):
-    if inputFile[-4:] == ".fun":
-        outFile = open(inputFile[:-4], 'w')
-        outFile.write(decryptedData)
-        outFile.close()
-    else:
-        outFile = open(inputFile, 'w')
-        outFile.write(decryptedData)
-        outFile.close()
+	if inputFile[-4:] == ".fun":
+		outFile = open(inputFile[:-4], 'w')
+		outFile.write(decryptedData)
+		outFile.close()
+	else:
+		outFile = open(inputFile, 'w')
+		outFile.write(decryptedData)
+		outFile.close()
 
 
 def main(argv):
-    try:
-        # Cheking for correct arguments
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-    except getopt.GetoptError:
-        print('jigsawRansomware.py -i <inputfile> -o <outputfile>')
-        sys.exit(2)
+	try:
+		# Cheking for correct arguments
+		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+	except getopt.GetoptError:
+		print('jigsawRansomware.py -<option>')
+		print('For help command is:')
+		print('jigsawRansomware.py -h')
+		sys.exit(2)
 
-    for opt, arg in opts:
-        if opt == '-h':
-            print('jigsawRansomware.py -i <inputfile> -o <outputfile>')
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputFile = arg
+	for opt, arg in opts:
+		if opt == '-h':
+			print('python jigsawRansomware.py -<option>')
+			print('\n<options>')
+			print("\n1. For decrypting a paricular file:\npython jigsawRansomware.py -i <inputfile> -o <outputfile>\n\n2. For deleting Registry Value:\npython jigsawRansomware.py -a")
+	
+			sys.exit()
+		elif opt == '-i':
+			inputFile = arg
+			encryptedData = readFile(inputFile)
+        		decryptedData = decryption.Decrypt(encryptedData)
+        		writeFile(inputFile, decryptedData)
 
-    encryptedData = readFile(inputFile)
-    decryptedData = decryption.Decrypt(encryptedData)
-    writeFile(inputFile, decryptedData)
-
+		elif opt == '-r':
+			ret = registryRemover.registryMain()
+			if ret == 1:
+				print("Registry Value Deleted Successfully")
+			else:
+				print("Cannot Delete Registry Value")
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	main(sys.argv[1:])
